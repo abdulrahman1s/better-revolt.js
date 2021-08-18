9406fc6fd52db1860aa5a190caea08ef47363a54
TheMaestro0 <abdovc012@gmail.com>
2021-08-18 07:11:11 +0200

    Fix: createCustomError

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -3,7 +3,8 @@ import { Messages } from './Messages'
 const createCustomError = (Base: ErrorConstructor) => {
     return class RevoltError<K extends keyof typeof Messages = keyof typeof Messages> extends Base {
         constructor(key: K, ...args: Parameters<typeof Messages[K]>) {
-            super(Messages[key].call(null, ...args))
+            const msg = Messages[key] as (...args: unknown[]) => string
+            super(msg(...args))
             Base.captureStackTrace(this, RevoltError)
         }
     }
