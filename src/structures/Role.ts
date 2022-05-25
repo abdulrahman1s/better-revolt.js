d23de3c69af1bf40474565792f0052dbe86bf905
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-05-25 21:31:35 +0200

    feat(Role): Add #overwrite field

--- src/structures/Role.ts
+++ src/structures/Role.ts
@@ -1,11 +1,12 @@
 import { Role as APIRole } from 'revolt-api'
-import { Base, Server } from './index'
-import { UUID } from '../util'
+import { Base, Server, Overwrite } from './index'
+import { UUID, ChannelPermissions } from '../util/index'
 
 export class Role extends Base {
     name!: string
     color: string | null = null
     hoist = false
+    overwrite!: Overwrite
     constructor(public server: Server, data: APIRole & { id: string }) {
         super(server.client)
         this._patch(data)
@@ -13,9 +14,15 @@ export class Role extends Base {
 
     protected _patch(data: APIRole & { _id?: string }): this {
         super._patch(data)
+        
         if (data.name) this.name = data.name
         if (typeof data.hoist === 'boolean') this.hoist = data.hoist
         if ('colour' in data) this.color = data.colour ?? null
+        if (data.permissions) {
+            const { a, d } = data.permissions
+            this.overwrite = { allow: new ChannelPermissions(a), deny: new ChannelPermissions(d) }
+        }
+
         return this
     }
 
@@ -25,6 +32,10 @@ export class Role extends Base {
 
     get createdTimestamp(): number {
         return this.createdAt.getTime()
+    }
+
+    get permissions(): Overwrite {
+        return this.overwrite
     }
 
     async delete(): Promise<void> {
